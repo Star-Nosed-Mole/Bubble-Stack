cdb68d39137c26481c492c65ab00a72e0399a623
Sean Haverstockgit config --global user.name Sean <seanhaverstock@gmail.com>
2020-07-25 21:45:19 -0700

    navbar onClick and bubble render in MakeMap

--- client/components/map.tsx
+++ client/components/map.tsx
@@ -3,33 +3,37 @@ import Bubble from './Bubble';
 
 function Map() {
   const [data, setData] = useState({
-    data: {
-      name: 'Bubble-Stack',
-      children: [
-        {
-          name: 'React',
-          color: 'hsl(191, 70%, 50%)',
-          loc: 13285
-        }
-      ]
-    }
+    name: 'React',
+    children: [
+      {
+        name: 'Redux',
+        color: 'hsl(191, 70%, 50%)',
+        loc: 13285
+      },
+      {
+        name: 'Bootstrap',
+        color: 'hsl(191, 70%, 50%)',
+        loc: 13285
+      }
+    ]
   });
 
-  componentDidMount() {
-    fetch('/api/')
-      .then(res => res.json())
-      .then((query) => {
-        if (!Array.isArray(query)) characters = [];
-        return this.setState({
-          characters,
-          fetchedTechs: true
-        });
-      })
-      .catch(err => console.log('Map.componentDidMount: ERROR: ', err));
-  }
+  // componentDidMount() {
+  //   fetch('/api/')
+  //     .then(res => res.json())
+  //     .then((query) => {
+  //       if (!Array.isArray(query)) characters = [];
+  //       return this.setState({
+  //         characters,
+  //         fetchedTechs: true
+  //       });
+  //     })
+  //     .catch(err => console.log('Map.componentDidMount: ERROR: ', err));
+  // }
 
   return (
     <div className="bubbleContainer">
+      {console.log('IN MAP: ', data)}
       <Bubble data={data} />
     </div>
   );
